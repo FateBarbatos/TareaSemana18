1032a787fc5bbff8e72736260834c4be17b7dbc2
Jose Alcides <jg9072555@gmail.com>
2023-11-22 12:06:17 -0800

    Cambio  de motor sql

--- Practica_18/PracticaSem18/PracticaSem18/settings.py
+++ Practica_18/PracticaSem18/PracticaSem18/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'PracticaSem18.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME':'Hospital',
+        'USER':'postgres',
+        'PASSWORD':'daddyissues',
+        'HOST':'localhost',
+        'PORT':'5432'
+
     }
 }
 
